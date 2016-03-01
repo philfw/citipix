5f998d5a5ac5540259ddc4ec3c5ab19d8e9cfd35
philfw <philfw@gmail.com>
2016-02-29 22:44:19 -0500

    streamlined!

--- js/index.js
+++ js/index.js
@@ -1,23 +1,20 @@
-$('.entry').on('submit', function(event){
+$( document ).ready(function() {
+	$('#city-options').val('');
+});
+
+var cities = ['nyc','sf','la','atx','syd']
+
+for (var i=0; i<cities.length; i++) {
+	$('#city-options').append('<option value="'+cities[i]+'">'+cities[i]+'</option>');
+};
+
+$('select#city-options').on('change', function(event){
 	event.preventDefault();
 	changeBg();
-	$('#city-type').val('');
 });
 
 function changeBg (){
-	var entry = $('#city-type').val();
-	if (entry === 'new york'||entry ==='new york city'||entry ==='nyc'){
-		$('body').attr('class', 'nyc');
-	} else if (entry === 'san francisco'||entry==='sf'||entry==='bay area'){
-		$('body').attr('class', 'sf');
-	} else if (entry === 'la'||entry==='lax'||entry==='los angeles'){
-		$('body').attr('class', 'la');
-	} else if (entry === 'austin'||entry==='atx'){
-		$('body').attr('class', 'austin');
-	} else if (entry === 'sydney'||entry==='syd'){
-		$('body').attr('class', 'sydney');
-	} else {
-		$('body').css('background', 'background: url(../images/citipix_skyline.jpg) no-repeat center center fixed');
-	};
+	var entry = $('#city-options').val().toLowerCase();
+	$('body').attr('class', entry);
 };
 
